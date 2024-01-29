51c80f16de2ad7b47e9077162ca663474d2dcdb6
isatsam <=>
2024-01-29 20:56:15 +0300

    Remove test strings

--- app.py
+++ app.py
@@ -67,9 +67,7 @@ def amongsays(text):
 
 def main():
     if __name__ == '__main__':
-        #user_input = input()
-        user_input = "I like to creep around my home and act like a goblin. I don’t know why but I just enjoy doing this. Maybe it’s my way of dealing with stress or something but I just do it about once every week. Generally I’ll carry around a sack and creep around in a sort of crouch-walking position making goblin noises, then I’ll walk around my house and pick up various different “trinkets” and put them in my bag while saying stuff like “I’ll be having that” and laughing maniacally in my goblin voice (“trinkets” can include anything from shit I find on the ground to cutlery or other utensils). The other day I was talking with my neighbours and they mentioned hearing weird noises like what I wrote about and I was just internally screaming the entire conversation. I’m 99% sure they don’t know it’s me but god that 1% chance is seriously weighing on my mind."
-        #user_input = "hi"
+        user_input = input()
         amongsays(user_input)
 
 
